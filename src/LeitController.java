0753e96011a7c9cc9f3463e5157840d8534fbbf3
arnielmar <aeh25@hi.is>
2020-03-02 13:15:00 +0000

    Day trips

--- src/LeitController.java
+++ src/LeitController.java
@@ -2,6 +2,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Spinner;
+import javafx.scene.layout.AnchorPane;
 
 public class LeitController {
     public ComboBox flightFromCombobox;
@@ -10,4 +11,13 @@ public class LeitController {
     public DatePicker flightReturningDate;
     public Spinner flightPeople;
     public Button flugSearchButton;
+    public AnchorPane hotelSearchButton;
+    public ComboBox hotelComboBox;
+    public DatePicker hotelArrivalDate;
+    public DatePicker hotelDepartureDate;
+    public Spinner hotelPeople;
+    public ComboBox dayTripCombobox;
+    public DatePicker dayTripDate;
+    public Spinner dayTripPeople;
+    public Button dayTripSearch;
 }
