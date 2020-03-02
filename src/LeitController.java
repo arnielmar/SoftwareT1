5b914fe7fdc4789a07f6c44eddb8693371b7ce62
arnielmar <aeh25@hi.is>
2020-03-02 12:59:46 +0000

    Flight

--- src/LeitController.java
+++ src/LeitController.java
@@ -1,2 +1,13 @@
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Spinner;
+
 public class LeitController {
+    public ComboBox flightFromCombobox;
+    public ComboBox flightToComboBox;
+    public DatePicker flightDepartureDate;
+    public DatePicker flightReturningDate;
+    public Spinner flightPeople;
+    public Button flugSearchButton;
 }
