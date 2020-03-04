fc7e457017197a35caea88cc4748472203166b0d
Hallur Kristinn Hallsson <hkh32@hi.is>
2020-03-04 13:34:25 +0000

    skjölun

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Leit.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Utlit/Leit.fxml"));
         primaryStage.setTitle("Search Engine");
         primaryStage.setScene(new Scene(root, 550, 400));
         primaryStage.show();
