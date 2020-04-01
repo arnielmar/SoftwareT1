9e7e4bbf1a66bd5c2e136c9af4e2a798020c4381
arnielmar <aeh25@hi.is>
2020-04-01 17:53:00 +0000

    Orders útlit, aeh25

--- src/Utlit/OrderTabController.java
+++ src/Utlit/OrderTabController.java
@@ -1,11 +1,21 @@
 package Utlit;
 
+import Vinnsla.DayTrip;
+import Vinnsla.Flight;
+import Vinnsla.Hotel;
+import Vinnsla.Package;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 
 public class OrderTabController {
     @FXML
-    private ListView ordersListView;
+    private ListView<Flight> ordersListViewFlights;
+    @FXML
+    private ListView<Hotel> ordersListViewHotels;
+    @FXML
+    private ListView<DayTrip> ordersListViewDayTrips;
+    @FXML
+    private ListView<Package> ordersListViewPackages;
 
     public void initialize() {
         setjaLista();
