4a3bc1bbb11fd489dd23861af436134dfb7ff8ce
Hallur Kristinn Hallsson <hkh32@hi.is>
2020-04-01 16:00:48 +0000

    visible

--- src/Utlit/DayTripTabController.java
+++ src/Utlit/DayTripTabController.java
@@ -1,13 +1,17 @@
 package Utlit;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Spinner;
+import javafx.scene.control.*;
 
 public class DayTripTabController {
+    @FXML
+    private Label dayTripPlaceLabel;
+    @FXML
+    private Label dayTripHeader;
+    @FXML
+    private Label dayTripPerLabel;
     @FXML
     private ComboBox dayTripCombo;
     @FXML
@@ -16,4 +20,31 @@ public class DayTripTabController {
     private Button dayTripSearchButton;
     @FXML
     private Spinner dayTripPersons;
+
+
+public void leitHandler(ActionEvent actionevent)
+{
+    if (dayTripSearchButton.getText().equals("Search"))
+    {
+        dayTripSearchButton.setText("Back");
+        dayTripHeader.setText("Choose Day Trip");
+
+        dayTripPlaceLabel.setVisible(false);
+        dayTripPerLabel.setVisible(false);
+        dayTripCombo.setVisible(false);
+        dayTripDate.setVisible(false);
+        dayTripPersons.setVisible(false);
+
+    } else
+    {
+        dayTripSearchButton.setText("Search");
+        dayTripHeader.setText("Search for Day Trips");
+
+        dayTripPlaceLabel.setVisible(true);
+        dayTripPerLabel.setVisible(true);
+        dayTripCombo.setVisible(true);
+        dayTripDate.setVisible(true);
+        dayTripPersons.setVisible(true);
+    }
+}
 }
